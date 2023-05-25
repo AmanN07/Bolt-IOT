2e2325d1b28e768702bf056fd52b9ba85a570fee
Aman Netam <netama07@gmail.com>
2023-05-25 12:42:38 +0530

    Added code just for testing.

---
+++ MY-Project/Alarm.py
@@ -0,0 +1,27 @@
+from playsound import playsound
+from datetime import datetime
+
+print("module imported")
+
+
+alarm_time = input("Enter the time of alarm to be set:HH:MM:SS\n")
+alarm_hour=alarm_time[0:2]
+alarm_minute=alarm_time[0:2]
+alarm_seconds=alarm_time[6:8]
+alarm_period = alarm_time[9:11].upper()
+
+print("Setting up alarm..")
+while True:
+	now = datetime.now()
+	current_hour = now.strftime("%I")
+	current_minute = now.strftime("%M")
+	current_seconds = now.strftime("%S")
+	current_period = now.strftime("%p")
+	if(alarm_period==current_period):
+		if(alarm_hour==current_hour):
+			if(alarm_minute==current_minute):
+				if(alarm_seconds==current_seconds):
+					print("Wake Up!")
+					playsound('/Users/aman_07/Music/Music/Media.localized/Music/Akhil/Perfect - Akhil/Perfect_Akhil.mp3')
+					break
+
