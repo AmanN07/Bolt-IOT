49df8e8cb9f606e59c004f778618952241a8cb82
Aman Netam <netama07@gmail.com>
2023-05-26 09:45:38 +0530

    Removed the personal keys.

--- Wifi-module Operations/Restarting_wifimodule.py
+++ Wifi-module Operations/Restarting_wifimodule.py
@@ -2,8 +2,8 @@ print("Restarting the wifi module using the python library...")
 
 
 from boltiot import Bolt
-api_key = "6d84b61e-c5ae-4e6b-aaa7-4201162d906c"
-device_id  = "BOLT5436703"
+api_key = "XXXXXXXXXb-aaa7-4201162d906c"
+device_id  = "BOLTXXXX703"
 mybolt = Bolt(api_key, device_id)
 response = mybolt.restart()
 print (response, "\n")
