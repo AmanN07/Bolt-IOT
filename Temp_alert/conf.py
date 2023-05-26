7ab5675712867230a92e7de3aead7665484258f9
Aman Netam <netama07@gmail.com>
2023-05-26 07:29:17 +0530

    This directory connects a telegram bot so that we could could get notified using telegram.

---
+++ Temp_alert/conf.py
@@ -0,0 +1,8 @@
+"""Configurations for telegram_alert.py"""
+
+
+api_key = "6d84b61e-c5ae-4e6b-aaa7-4201162d906c"
+device_id  = "BOLT5436703"
+telegram_chat_id = "@https://t.me/temperature_alert_usingIOT" 
+telegram_bot_id = "bot5029551100:AAFIbp2JHD3Wnmsz5oMj7H9HsML2OXwqjTo" 
+threshold = 255 
