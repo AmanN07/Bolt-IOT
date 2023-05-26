e6fc0455707067bdf323cfd67e5bf233219e7373
Aman Netam <netama07@gmail.com>
2023-05-26 09:39:28 +0530

    Removed the personal api_key and device_id.

--- Wifi-module Operations/bolt_wifi_status.py
+++ Wifi-module Operations/bolt_wifi_status.py
@@ -2,8 +2,8 @@ print("Checking the status of the BOLT wifi module..... \n")
 
 
 from boltiot import Bolt
-api_key = "6d84b61e-c5ae-4e6b-aaa7-4201162d906c"
-device_id  = "BOLT5436703"
+api_key = "XXXXXXXXXXX-4e6b-aaa7-4201162d906c"
+device_id  = "BOLTXXXX703"
 mybolt = Bolt(api_key, device_id)
 response = mybolt.isOnline()
 print (response, "\n")
