10be0b88a1d4740b0c2883b6bbf2c1c8b7f7f666
Aman Netam <netama07@gmail.com>
2023-05-26 09:34:23 +0530

    Removed the pesonal telegram bot id.

--- Temp_alert/conf.py
+++ Temp_alert/conf.py
@@ -4,5 +4,5 @@
 api_key = "XXXXXXXX-4e6b-aaa7-4201162d906c"
 device_id  = "BOLTXXXX703"
 telegram_chat_id = "@https://t.me/temperature_alert_usingIOT" 
-telegram_bot_id = "bot502955XXXX:AAFIbp2JHD3Wnmsz5oMj7H9HsML2OXwqjTo" 
+telegram_bot_id = "bot502955XXXX:XXXXXXXXX5oMj7H9HsML2OXwqjTo" 
 threshold = 255 
