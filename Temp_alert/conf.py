4df88ebdcc2335a756d27965fac9d0ad3e901af0
Aman Netam <netama07@gmail.com>
2023-05-26 07:55:36 +0530

    Removed the Personal api_key and device id.

--- Temp_alert/conf.py
+++ Temp_alert/conf.py
@@ -1,8 +1,8 @@
 """Configurations for telegram_alert.py"""
 
 
-api_key = "6d84b61e-c5ae-4e6b-aaa7-4201162d906c"
-device_id  = "BOLT5436703"
+api_key = "XXXXXXXX-4e6b-aaa7-4201162d906c"
+device_id  = "BOLTXXXX703"
 telegram_chat_id = "@https://t.me/temperature_alert_usingIOT" 
-telegram_bot_id = "bot5029551100:AAFIbp2JHD3Wnmsz5oMj7H9HsML2OXwqjTo" 
+telegram_bot_id = "bot502955XXXX:AAFIbp2JHD3Wnmsz5oMj7H9HsML2OXwqjTo" 
 threshold = 255 
