e807bdf9eb3e3c3a8fa714e39e71c1868adaded4
Aman Netam <netama07@gmail.com>
2023-05-26 09:59:08 +0530

    This directory contains code that implement Anomaly detection.

---
+++ Anomaly_Detection/conf.py
@@ -0,0 +1,10 @@
+# Configuration required for anomaly detection...
+
+SSID = 'XXXXXXXXXXXXXXXXX484156c3a2a28557' 
+AUTH_TOKEN = 'XXXXXXXXXXXXXXX930a3efa20ee265be64' 
+FROM_NUMBER = 'XXXXXXX8'
+TO_NUMBER = '+91XXXXXX0XX'
+API_KEY = 'XXXXXXXXXX-aaa7-4201162d906c'
+DEVICE_ID = 'BOLTXXXX703'
+FRAME_SIZE = 5          # this is used for choosing staring point
+MUL_FACTOR = 3          # this is used for selecting the distance from the original data 
